fe7a77b45f8ca0392b1485d59ea35d819970b146
giansalex <giansalex@gmail.com>
2017-10-03 12:24:47 -0500

    Add get Status Cdr

--- src/Greenter/Ws/Services/ExtService.php
+++ src/Greenter/Ws/Services/ExtService.php
@@ -8,6 +8,7 @@
 
 namespace Greenter\Ws\Services;
 
+use Greenter\Model\Response\BillResult;
 use Greenter\Model\Response\StatusResult;
 
 /**
@@ -45,4 +46,42 @@ class ExtService extends BaseSunat
 
         return $result;
     }
+
+    /**
+     * @param string $ruc
+     * @param string $tipo
+     * @param string $serie
+     * @param string $numero
+     * @return BillResult
+     */
+    public function getCdrStatus($ruc, $tipo, $serie, $numero)
+    {
+        $client = $this->getClient();
+        $result = new BillResult();
+
+        try {
+            $params = [
+                'rucComprobante' => $ruc,
+                'tipoComprobante' => $tipo,
+                'serieComprobante' => $serie,
+                'numeroComprobante' => $numero,
+            ];
+            $response = $client->call('getStatusCdr', [ 'parameters' => $params ]);
+            $statusCdr =$response->statusCdr;
+
+//            $code = $statusCdr->statusCode;
+//            $msg = $statusCdr->statusMessage;
+            $cdrZip = $statusCdr->content;
+            $result
+                ->setCdrResponse($this->extractResponse($cdrZip))
+                ->setCdrZip($cdrZip)
+                ->setSuccess(true);
+        }
+        catch (\SoapFault $e) {
+            $result->setError($this->getErrorFromFault($e));
+        }
+
+        return $result;
+    }
+
 }
