70e55474266de7ec299abe08058f2efda79b869b
Giancarlos Salas <giansalex@gmail.com>
2017-10-05 08:03:34 -0500

    Add document parser

---
+++ packages/core/src/Core/Parser/DocumentParserInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 05/10/2017
+ * Time: 08:01
+ */
+
+namespace Greenter\Parser;
+
+use Greenter\Model\DocumentInterface;
+
+/**
+ * Interface DocumentParserInterface
+ * @package Greenter\Parser
+ */
+interface DocumentParserInterface
+{
+    /**
+     * @param $value
+     * @return DocumentInterface
+     */
+    public function parse($value);
+}
