ed3502a41f9ecec4c6271d4c550568c7297b1dad
giansalex <giansalex@gmail.com>
2017-11-06 10:59:17 -0500

    add despatch parser, close #2

---
+++ packages/xml-parser/tests/Xml/Parser/DespatchParserTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrador
+ * Date: 06/11/2017
+ * Time: 10:41 AM
+ */
+
+namespace Tests\Greenter\Xml\Parser;
+
+use Greenter\Model\Despatch\Despatch;
+use Greenter\Xml\Parser\DespatchParser;
+
+/**
+ * Class DespatchParserTest
+ * @package Tests\Greenter\Xml\Parser
+ */
+class DespatchParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getGuiasFile
+     * @param string $filename
+     */
+    public function testGuias($filename)
+    {
+        $parser = new DespatchParser();
+        $xml = file_get_contents($filename);
+        /**@var $obj Despatch */
+        $obj = $parser->parse($xml);
+
+        $this->assertEquals('09', $obj->getTipoDoc());
+        $this->assertRegExp('/^T\w{3}/', $obj->getSerie());
+        $this->assertNotNull($obj->getCompany());
+        $this->assertNotNull($obj->getDestinatario());
+        $this->assertLessThanOrEqual(8, strlen($obj->getCorrelativo()));
+        $this->assertGreaterThan(0, count($obj->getDetails()));
+    }
+
+    public function getGuiasFile()
+    {
+        $files = glob(__DIR__.'/../../Resources/guias/*.xml');
+
+        return array_map(function ($file) {
+            return [$file];
+        }, $files);
+    }
+}
