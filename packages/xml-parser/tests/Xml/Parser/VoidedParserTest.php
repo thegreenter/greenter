ae9ea1e5864eec62b0312235484321f9b328a726
Giancarlos Salas <giansalex@gmail.com>
2017-11-08 21:58:46 -0500

    added Voided/Reversion Parser , close #3

---
+++ packages/xml-parser/tests/Xml/Parser/VoidedParserTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 08/11/2017
+ * Time: 21:44
+ */
+
+namespace Tests\Greenter\Xml\Parser;
+
+use Greenter\Model\Voided\Voided;
+use Greenter\Xml\Parser\VoidedParser;
+
+class VoidedParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider providerDocs
+     * @param string $filename
+     */
+    public function testParseDoc($filename)
+    {
+        $xml = file_get_contents($filename);
+        /**@var $doc Voided */
+        $doc = $this->getParser()->parse($xml);
+
+        $this->assertLessThanOrEqual(5, strlen($doc->getCorrelativo()));
+        $this->assertLessThanOrEqual(new \DateTime(), $doc->getFecComunicacion());
+        $this->assertLessThanOrEqual(new \DateTime(), $doc->getFecGeneracion());
+        $this->assertGreaterThanOrEqual(1, count($doc->getDetails()));
+    }
+
+    public function providerDocs()
+    {
+        $files = glob(__DIR__.'/../../Resources/bajas/*.xml');
+
+        return array_map(function ($file) {
+            return [$file];
+        }, $files);
+    }
+
+    private function getParser()
+    {
+        return new VoidedParser();
+    }
+}
