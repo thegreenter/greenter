98588875b3347a7bcdf0ea3c62689ce4d9420691
Giancarlos Salas <giansalex@gmail.com>
2018-01-27 21:10:34 -0500

    add error codes repo

---
+++ packages/xcodes/README.md
@@ -0,0 +1,12 @@
+# XCodes - Greenter
+
+[![Travis-CI](https://img.shields.io/travis/giansalex/greenter-ws.svg?label=build&branch=master&style=flat-square)](https://travis-ci.org/giansalex/greenter-ws)
+[![Coverage Status](https://img.shields.io/coveralls/giansalex/greenter-ws.svg?label=coveralls&style=flat-square&branch=master)](https://coveralls.io/github/giansalex/greenter-ws?branch=master)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/64cabd82882a461dbf82bdeb6accbc13)](https://www.codacy.com/app/giansalex/greenter-ws?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=giansalex/greenter-ws&amp;utm_campaign=Badge_Grade)  
+Códigos de Error en Facturación Electrónica SUNAT.
+
+# Install
+Via Composer from [packagist.org](https://packagist.org/packages/greenter/xcodes).
+```bash
+composer require greenter/xcodes
+```
---
+++ packages/xcodes/tests/Validator/XmlErrorCodeProviderTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 27/01/2018
+ * Time: 21:01
+ */
+
+namespace Tests\Greenter\Validator;
+
+
+use Greenter\Validator\ErrorCodeProviderInterface;
+use Greenter\Validator\XmlErrorCodeProvider;
+
+class XmlErrorCodeProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ErrorCodeProviderInterface
+     */
+    private $provider;
+
+    protected function setUp()
+    {
+        $this->provider = new XmlErrorCodeProvider();
+    }
+
+    public function testGetAllCodes()
+    {
+        $items = $this->provider->getAll();
+
+        $this->assertEquals(1141, count($items));
+    }
+
+    /**
+     * @dataProvider providerCodes
+     * @param string $code
+     */
+    public function testGetErrorMessage($code)
+    {
+        $msg = $this->provider->getValue($code);
+
+        $this->assertNotEmpty($msg);
+    }
+
+    /**
+     * @dataProvider providerInvalidCodes
+     * @param string $code
+     */
+    public function testGetErrorMessageEmpty($code)
+    {
+        $msg = $this->provider->getValue($code);
+
+        $this->assertEmpty($msg);
+    }
+
+    public function providerCodes()
+    {
+        return [
+          ['0110'],
+          ['0200'],
+          ['0404'],
+          ['1055'],
+          ['2630'],
+          ['4035'],
+          ['4230'],
+        ];
+    }
+
+    public function providerInvalidCodes()
+    {
+        return [
+            ['A110'],
+            ['B200'],
+            ['C404'],
+            ['D002'],
+            ['E011'],
+            ['F200'],
+            ['G004'],
+        ];
+    }
+}
