234e97a2c4c04d44a9223c7116beca4bcdb70369
Giancarlos Salas <giansalex@gmail.com>
2018-01-28 19:29:48 -0500

    add invoice full

--- packages/data/src/Data/StoreTrait.php
+++ packages/data/src/Data/StoreTrait.php
@@ -22,7 +22,9 @@ use Greenter\Model\Retention\Exchange;
 use Greenter\Model\Retention\Payment;
 use Greenter\Model\Retention\Retention;
 use Greenter\Model\Retention\RetentionDetail;
+use Greenter\Model\Sale\Detraction;
 use Greenter\Model\Sale\Document;
+use Greenter\Model\Sale\EmbededDespatch;
 use Greenter\Model\Sale\Invoice;
 use Greenter\Model\Sale\Legend;
 use Greenter\Model\Sale\Note;
@@ -135,6 +137,106 @@ trait StoreTrait
         return $invoice;
     }
 
+    public function getFullInvoice()
+    {
+        $invoice = new Invoice();
+        $invoice
+            ->setFecVencimiento(new \DateTime())
+            ->setMtoOperGratuitas(12)
+            ->setSumDsctoGlobal(12)
+            ->setMtoDescuentos(23)
+            ->setTipoOperacion('2')
+            ->setPerception((new SalePerception())
+                ->setCodReg('01')
+                ->setMto(2)
+                ->setMtoBase(3)
+                ->setMtoTotal(4)
+            )->setCompra('001-12112')
+            ->setDetraccion((new Detraction())
+                ->setMount(2228.3)
+                ->setPercent(9)
+                ->setValueRef(2000)
+            )->setGuiaEmbebida((new EmbededDespatch())
+                ->setLlegada(new Direction('070101', 'AV. REPUBLICA DE ARGENTINA N? 2976 URB.'))
+                ->setPartida(new Direction('070101', 'AV OSCAR R BENAVIDES No 5915  PE'))
+                ->setTransportista((new Client())
+                    ->setTipoDoc('6')
+                    ->setNumDoc('20100006376')
+                    ->setRznSocial('TRANS SAC')
+                )->setNroLicencia('1111111111')
+                ->setTranspPlaca('B9Y-778')
+                ->setTranspCodeAuth('112121')
+                ->setTranspMarca('Scania')
+                ->setModTraslado('01')
+                ->setUndPesoBruto('KGM')
+                ->setPesoBruto(2020.23)
+            )->setAnticipos([(new Prepayment())
+                ->setTotal(100)
+                ->setTipoDocRel('02')
+                ->setNroDocRel('F001-21'),
+                (new Prepayment())->setTotal(200)
+                    ->setTipoDocRel('02')
+                    ->setNroDocRel('F001-23')
+            ])->setTotalAnticipos(300)
+            ->setRelDocs([(new Document())
+                ->setTipoDoc('01')
+                ->setNroDoc('F001-123')
+            ])
+            ->setGuias([(new Document())
+                ->setTipoDoc('09')
+                ->setNroDoc('T001-1')
+            ])
+            ->setTipoDoc('01')
+            ->setSerie('F001')
+            ->setCorrelativo('123')
+            ->setFechaEmision(new \DateTime())
+            ->setTipoMoneda('PEN')
+            ->setClient($this->getClient())->setMtoOperGravadas(200)
+            ->setMtoOperExoneradas(0)
+            ->setMtoOperInafectas(0)
+            ->setMtoIGV(36)
+            ->setMtoISC(2)
+            ->setSumOtrosCargos(12)
+            ->setMtoOtrosTributos(1)
+            ->setMtoImpVenta(236)
+            ->setCompany($this->getCompany())
+            ->setDetails([(new SaleDetail())
+                ->setCodProducto('C023')
+                ->setUnidad('NIU')
+                ->setCantidad(2)
+                ->setDescripcion('PROD 1')
+                ->setIgv(18)
+                ->setIsc(3)
+                ->setTipSisIsc('3')
+                ->setMtoValorGratuito(12)
+                ->setTipAfeIgv('10')
+                ->setMtoValorVenta(100)
+                ->setMtoValorUnitario(50)
+                ->setMtoPrecioUnitario(56)
+                , (new SaleDetail())
+                    ->setCodProducto('C02')
+                    ->setCodProdSunat('001')
+                    ->setUnidad('NIU')
+                    ->setCantidad(2)
+                    ->setDescripcion('PROD 2')
+                    ->setDescuento(1)
+                    ->setTipSisIsc('3')
+                    ->setIsc(1)
+                    ->setIgv(18)
+                    ->setTipAfeIgv('10')
+                    ->setMtoValorVenta(100)
+                    ->setMtoValorUnitario(10)
+                    ->setMtoValorGratuito(2)
+                    ->setMtoPrecioUnitario(0)
+            ])->setLegends([
+                (new Legend())
+                    ->setCode('1000')
+                    ->setValue('SON N CON 00/100 SOLES')
+            ]);
+
+        return $invoice;
+    }
+
     public function getNote()
     {
         $note = new Note();
@@ -220,7 +322,7 @@ trait StoreTrait
         $reversion->setCorrelativo('001')
             ->setFecComunicacion(new \DateTime())
             ->setFecGeneracion(new \DateTime())
-            ->setCompany(Util::getCompany())
+            ->setCompany($this->getCompany())
             ->setDetails([$detial1, $detial2]);
 
         return $reversion;
