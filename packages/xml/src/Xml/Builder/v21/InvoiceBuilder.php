8180168023551884efc681203f254aad6b9844ff
Giancarlos Salas <giansalex@gmail.com>
2018-10-05 16:05:28 -0500

    ad invoice builder

---
+++ packages/xml/src/Xml/Builder/v21/InvoiceBuilder.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Soporte
+ * Date: 5/10/2018
+ * Time: 13:53
+ */
+
+namespace Greenter\Xml\Builder\v21;
+
+use Greenter\Builder\BuilderInterface;
+use Greenter\Model\DocumentInterface;
+use Greenter\Xml\Builder\TwigBuilder;
+
+/**
+ * Class InvoiceBuilder
+ * @package Greenter\Xml\Builder
+ */
+class InvoiceBuilder extends TwigBuilder implements BuilderInterface
+{
+
+    /**
+     * Create xml for document.
+     *
+     * @param DocumentInterface $document
+     * @return string
+     */
+    public function build(DocumentInterface $document)
+    {
+        return $this->render('invoice.21.xml.twig', $document);
+    }
+}
