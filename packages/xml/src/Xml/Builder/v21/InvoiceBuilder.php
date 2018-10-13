2c36719cbf5e44fae57c9b6330ad90332f247533
Giancarlos Salas <giansalex@gmail.com>
2018-10-13 17:48:49 -0500

    update twig close tag

--- packages/xml/src/Xml/Builder/v21/InvoiceBuilder.php
+++ packages/xml/src/Xml/Builder/v21/InvoiceBuilder.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: Soporte
  * Date: 5/10/2018
- * Time: 13:53
+ * Time: 13:53.
  */
 
 namespace Greenter\Xml\Builder\v21;
@@ -13,16 +13,15 @@ use Greenter\Model\DocumentInterface;
 use Greenter\Xml\Builder\TwigBuilder;
 
 /**
- * Class InvoiceBuilder
- * @package Greenter\Xml\Builder
+ * Class InvoiceBuilder.
  */
 class InvoiceBuilder extends TwigBuilder implements BuilderInterface
 {
-
     /**
      * Create xml for document.
      *
      * @param DocumentInterface $document
+     *
      * @return string
      */
     public function build(DocumentInterface $document)
