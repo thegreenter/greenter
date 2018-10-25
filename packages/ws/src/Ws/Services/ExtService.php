06bd1733dbca1f21af984835b55cd0d072b5a943
Giancarlos Salas <giansalex@gmail.com>
2018-10-24 22:43:04 -0500

    get error from cdr

--- packages/ws/src/Ws/Services/ExtService.php
+++ packages/ws/src/Ws/Services/ExtService.php
@@ -43,7 +43,14 @@ class ExtService extends BaseSunat
                 $result
                     ->setCdrResponse($this->extractResponse($cdrZip))
                     ->setCdrZip($cdrZip);
+
+                $code = $result->getCdrResponse()->getCode();
             }
+
+            if ($this->isExceptionCode($code)) {
+                $this->loadErrorByCode($result, $code);
+            }
+
         } catch (\SoapFault $e) {
             $result->setError($this->getErrorFromFault($e));
         }
