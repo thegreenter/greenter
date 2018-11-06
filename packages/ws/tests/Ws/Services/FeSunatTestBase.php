b8bf41e340ccc1c7cab3078611d8e517f234f609
Giancarlos Salas <giansalex@gmail.com>
2018-11-06 00:37:09 -0500

    remove cdr code from ExtService

--- packages/ws/tests/Ws/Services/FeSunatTestBase.php
+++ packages/ws/tests/Ws/Services/FeSunatTestBase.php
@@ -34,19 +34,12 @@ abstract class FeSunatTestBase extends \PHPUnit_Framework_TestCase
                     ->getMock();
 
         $stub->method('call')
-                ->will($this->returnCallback(function ($func, $params) {
+                ->will($this->returnCallback(function () {
                     $zipContent = file_get_contents(__DIR__.'/../../Resources/cdrBaja.zip');
                     $obj = new \stdClass();
-                    if ($func == 'getStatus') {
-                        $obj->status = new \stdClass();
-                        $obj->status->statusCode = '0';
-                        $obj->status->content = $zipContent;
-                    } elseif ($func == 'getStatusCdr') {
-                        $obj->statusCdr = new \stdClass();
-                        $obj->statusCdr->statusCode = '0';
-                        $obj->statusCdr->statusMessage = 'ACEPTADA';
-                        $obj->statusCdr->content = $zipContent;
-                    }
+                    $obj->status = new \stdClass();
+                    $obj->status->statusCode = '0';
+                    $obj->status->content = $zipContent;
 
                     return $obj;
                 }));
