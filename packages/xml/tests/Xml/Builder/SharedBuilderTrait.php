5b38f4371fd4541338db3e256a7e065ed39b25ba
Giancarlos Salas <giansalex@gmail.com>
2019-03-10 23:33:53 -0500

    add shared trait

---
+++ packages/xml/tests/Xml/Builder/SharedBuilderTrait.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 10/03/2019
+ * Time: 23:26
+ */
+
+namespace Tests\Greenter\Xml\Builder;
+
+use Greenter\Data\GeneratorFactory;
+use Greenter\Data\SharedStore;
+
+trait SharedBuilderTrait
+{
+    private function getShared()
+    {
+        return new SharedStore();
+    }
+
+    private function createDocument($type)
+    {
+        $factory = new GeneratorFactory();
+        $factory->shared = $this->getShared();
+        $generator = $factory->create($type);
+
+        return $generator->create();
+    }
+}
