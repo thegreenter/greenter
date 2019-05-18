5948d1a3159eb5ed739e4a1287da9a4c355ac373
Giancarlos Salas <giansalex@gmail.com>
2019-05-17 20:06:40 -0500

    register max limit decimal format

--- packages/xml/src/Xml/Builder/TwigBuilder.php
+++ packages/xml/src/Xml/Builder/TwigBuilder.php
@@ -3,24 +3,34 @@
  * Created by PhpStorm.
  * User: Giansalex
  * Date: 09/08/2017
- * Time: 19:23
+ * Time: 19:23.
  */
 
 namespace Greenter\Xml\Builder;
 
+use Greenter\Xml\Filter\FormatFilter;
+use Greenter\Xml\Filter\TributoFunction;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+use Twig_SimpleFilter;
+use Twig_SimpleFunction;
+
 /**
- * Class TwigBuilder
- * @package Greenter\Xml\Builder
+ * Class TwigBuilder.
  */
 class TwigBuilder
 {
     /**
-     * @var \Twig_Environment
+     * @var Twig_Environment
      */
     protected $twig;
 
     /**
      * TwigBuilder constructor.
+     *
      * @param array $options [optional] Recommended: 'cache' => '/dir/cache'
      */
     public function __construct($options = [])
@@ -33,26 +43,39 @@ class TwigBuilder
      *
      * @param string $template
      * @param object $doc
+     *
      * @return string
+     *
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function render($template, $doc)
     {
         return $this->twig->render($template, [
-            'doc' => $doc
+            'doc' => $doc,
         ]);
     }
 
     private function initTwig($options)
     {
-        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../Templates');
-        $numFilter = new \Twig_SimpleFilter('n_format', function ($number, $decimals = 2) {
-            return number_format($number, $decimals, '.', '');
-        });
+        $loader = new Twig_Loader_Filesystem(__DIR__.'/../Templates');
 
-        $twig = new \Twig_Environment($loader, $options);
-        $twig->addFilter($numFilter);
-        $twig->addFunction(new \Twig_SimpleFunction('getTributoAfect', ['Greenter\Xml\Filter\TributoFunction', 'getByAfectacion']));
+        $twig = new Twig_Environment($loader, $options);
+        $this->LoadFilterAndFunctions($twig);
 
         $this->twig = $twig;
+    }
+
+    /**
+     * @param Twig_Environment $twig
+     */
+    private function LoadFilterAndFunctions(Twig_Environment $twig)
+    {
+        $formatFilter = new FormatFilter();
+
+        $twig->addFilter(new Twig_SimpleFilter('n_format', [$formatFilter, 'number']));
+        $twig->addFilter(new Twig_SimpleFilter('n_format_limit', [$formatFilter, 'numberLimit']));
+        $twig->addFunction(new Twig_SimpleFunction('getTributoAfect', [TributoFunction::class, 'getByAfectacion']));
     }
 }
