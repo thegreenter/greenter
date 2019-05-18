3f0042ff715b662b7917b25339ea99b8570787ba
Giancarlos Salas <giansalex@gmail.com>
2019-05-17 19:50:16 -0500

    add number format filter

---
+++ packages/xml/src/Xml/Filter/FormatFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Greenter\Xml\Filter;
+
+/**
+ * Class FormatFilter.
+ */
+class FormatFilter
+{
+    public function number($number, $decimals = 2)
+    {
+        return number_format($number, $decimals, '.', '');
+    }
+
+    public function numberLimit($number, $decimals)
+    {
+        $nroDecimals = strlen(substr(strrchr($number, '.'), 1));
+
+        return $nroDecimals > $decimals ? $this->number($number, $decimals) : $number;
+    }
+}
