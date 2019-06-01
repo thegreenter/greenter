be44ae6fb6bc2ac9a3d0450dd21cbe03e4c51ce3
Giancarlos Salas <giansalex@gmail.com>
2019-06-01 09:44:22 -0500

    fix tests - store builder

--- packages/report/tests/Report/HtmlReportTest.php
+++ packages/report/tests/Report/HtmlReportTest.php
@@ -1,39 +1,164 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Giansalex
- * Date: 17/09/2017
- * Time: 22:17
+ * User: Administrador
+ * Date: 20/01/2018
+ * Time: 02:21 PM
  */
 
 namespace Tests\Greenter\Report;
 
-use Greenter\Data\StoreTrait;
+use Greenter\Data\Generator\DespatchStore;
+use Greenter\Data\Generator\InvoiceStore;
+use Greenter\Data\Generator\NoteStore;
+use Greenter\Data\Generator\PerceptionStore;
+use Greenter\Data\Generator\RetentionStore;
+use Greenter\Data\Generator\ReversionStore;
+use Greenter\Data\Generator\SummaryStore;
+use Greenter\Data\Generator\VoidedStore;
+use Greenter\Model\DocumentInterface;
 
-/**
- * Trait HtmlReportTest
- * @package Tests\Greenter\Report
- */
 class HtmlReportTest extends \PHPUnit_Framework_TestCase
 {
-    use StoreTrait;
     use HtmlReportTrait;
+    use SharedBuilderTrait;
+
+    /**
+     * @dataProvider provideDocs
+     * @param DocumentInterface $document
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function testGenInvoiceReport(DocumentInterface $document)
+    {
+        $report = $this->getReporter();
+        $report->setTemplate('invoice.html.twig');
+
+        $html = $report->render($document, $this->getParamters());
+        $this->assertNotEmpty($html);
+        $this->showResult($document->getName(), $html);
+    }
 
     /**
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function testGenReport()
+    public function testGenReportOtherTemplate()
     {
         $report = $this->getReporter();
-        $inv = $this->getInvoice();
+        $inv = $this->createDocument(InvoiceStore::class);
         $report->setTemplate('invoice2.html.twig');
 
         $html = $report->render($inv, $this->getParamters());
         $this->assertNotEmpty($html);
+    }
 
-//         file_put_contents('file.html', $html);
+    /**
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function testGenVoidedReport()
+    {
+        $report = $this->getReporter();
+        $report->setTemplate('voided.html.twig');
+
+        $document = $this->createDocument(VoidedStore::class);
+        $html = $report->render($document, $this->getDefaultParamters());
+        $this->assertNotEmpty($html);
+        $this->showResult($document->getName(), $html);
+    }
+
+    /**
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function testGenReversionReport()
+    {
+        $report = $this->getReporter();
+        $report->setTemplate('voided.html.twig');
+
+        $document = $this->createDocument(ReversionStore::class);
+        $html = $report->render($document, $this->getDefaultParamters());
+        $this->assertNotEmpty($html);
+        $this->showResult($document->getName(), $html);
+    }
+
+    /**
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function testGenSummaryReport()
+    {
+        $report = $this->getReporter();
+        $report->setTemplate('summary.html.twig');
+
+        $document = $this->createDocument(SummaryStore::class);
+        $html = $report->render($document, $this->getDefaultParamters());
+        $this->assertNotEmpty($html);
+        $this->showResult($document->getName(), $html);
+    }
+
+    /**
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function testGenRetentionReport()
+    {
+        $report = $this->getReporter();
+        $report->setTemplate('retention.html.twig');
+
+        $document = $this->createDocument(RetentionStore::class);
+        $html = $report->render($document, $this->getDefaultParamters());
+        $this->assertNotEmpty($html);
+        $this->showResult($document->getName(), $html);
+    }
+
+    /**
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function testGenPerceptionReport()
+    {
+        $report = $this->getReporter();
+        $report->setTemplate('perception.html.twig');
+
+        $document = $this->createDocument(PerceptionStore::class);
+        $html = $report->render($document, $this->getDefaultParamters());
+        $this->assertNotEmpty($html);
+        $this->showResult($document->getName(), $html);
+    }
+
+    /**
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function testGenDespatchReport()
+    {
+        $report = $this->getReporter();
+        $report->setTemplate('despatch.html.twig');
+
+        $document = $this->createDocument(DespatchStore::class);
+        $parameters = $this->getDefaultParamters();
+        unset($parameters['user']['footer']);
+        $html = $report->render($document, $parameters);
+        $this->assertNotEmpty($html);
+        $this->showResult($document->getName(), $html);
+    }
+
+    public function provideDocs()
+    {
+        return [
+          [$this->createDocument(InvoiceStore::class)],
+          [$this->createDocument(NoteStore::class)]
+        ];
     }
 
     private function getParamters()
@@ -43,9 +168,16 @@ class HtmlReportTest extends \PHPUnit_Framework_TestCase
         return [
             'system' => [
                 'logo' => $logo,
+                'hash' => 'xkhakjjuui293/=33w',
             ],
             'user' => [
-                'header' => 'Telf: <b>(056) 123375</b>'
+                'resolucion' => '212321',
+                'header' => 'Telf: <b>(056) 123375</b>',
+                'extras' => [
+                    ['name' => 'CONDICION DE PAGO', 'value' => 'Efectivo'],
+                    ['name' => 'VENDEDOR', 'value' => 'GITHUB SELLER'],
+                ],
+                'footer' => file_get_contents(__DIR__.'/../Resources/footer.html'),
             ]
         ];
     }
---
+++ packages/report/tests/Report/SharedBuilderTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Tests\Greenter\Report;
+
+use Greenter\Data\GeneratorFactory;
+use Greenter\Data\SharedStore;
+
+trait SharedBuilderTrait
+{
+    private function getShared()
+    {
+        return new SharedStore();
+    }
+
+    private function createDocument($type)
+    {
+        $factory = new GeneratorFactory();
+        $factory->shared = $this->getShared();
+        $generator = $factory->create($type);
+        return $generator->create();
+    }
+}
