a8f699052ce4c3c2e70d4ca7f9835df08a4c9b1a
Giancarlos Salas <giansalex@gmail.com>
2019-07-06 19:56:59 -0500

    Add Type attribute to password (security header)

--- packages/ws/src/Ws/Header/WSSESecurityHeader.php
+++ packages/ws/src/Ws/Header/WSSESecurityHeader.php
@@ -17,14 +17,17 @@ use SoapVar;
 class WSSESecurityHeader extends SoapHeader
 {
     const WSS_NAMESPACE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd';
+    const PASSWORD_TYPE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText';
+    const PASSWORD_FORMAT = '<o:Password xmlns:o="%s" Type="%s">%s</o:Password>';
 
     public function __construct($username, $password)
     {
+        $passwordXml = sprintf(self::PASSWORD_FORMAT, self::WSS_NAMESPACE, self::PASSWORD_TYPE, $password);
         $security = new SoapVar(
             [new SoapVar(
                 [
                     new SoapVar($username, XSD_STRING, null, null, 'Username', self::WSS_NAMESPACE),
-                    new SoapVar($password, XSD_STRING, null, null, 'Password', self::WSS_NAMESPACE),
+                    new SoapVar($passwordXml, XSD_ANYXML),
                 ],
                 SOAP_ENC_OBJECT,
                 null,
