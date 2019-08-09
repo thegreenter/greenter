2a566d5d1f418d4053eb41068c5e544974864b88
Giancarlos Salas <giansalex@gmail.com>
2019-08-09 11:44:52 -0500

    add github actions

--- packages/core/src/Core/Parser/DocumentParserInterface.php
+++ packages/core/src/Core/Parser/DocumentParserInterface.php
@@ -16,7 +16,7 @@ use Greenter\Model\DocumentInterface;
 interface DocumentParserInterface
 {
     /**
-     * @param $value
+     * @param mixed $value
      *
      * @return DocumentInterface
      */
