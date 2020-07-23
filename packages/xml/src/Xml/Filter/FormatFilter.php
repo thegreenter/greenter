c762a722fce31593f452f535aea184b8ae1d649f
Giancarlos Salas <giansalex@gmail.com>
2020-07-23 16:48:47 -0500

    xml format filter validate type

--- packages/xml/src/Xml/Filter/FormatFilter.php
+++ packages/xml/src/Xml/Filter/FormatFilter.php
@@ -8,15 +8,25 @@ namespace Greenter\Xml\Filter;
  */
 class FormatFilter
 {
-    public function number($number, $decimals = 2): string
+    public function number($number, $decimals = 2): ?string
     {
         return number_format($number, $decimals, '.', '');
     }
 
-    public function numberLimit($number, $decimals)
+    public function numberLimit($number, $decimals): ?string
     {
-        $nroDecimals = strlen(substr(strrchr($number, '.'), 1));
+        $nroDecimals = $this->getDecimalsLenght($number);
 
-        return $nroDecimals > $decimals ? $this->number($number, $decimals) : $number;
+        return $nroDecimals > $decimals ? $this->number($number, $decimals) : strval($number);
+    }
+
+    private function getDecimalsLenght($number): int
+    {
+        $lasPosition = strrchr(strval($number), '.');
+        if ($lasPosition === false) {
+            return 0;
+        }
+
+        return strlen(substr($lasPosition, 1));
     }
 }
