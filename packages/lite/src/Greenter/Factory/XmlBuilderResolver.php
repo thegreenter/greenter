05df2825dbe75412f80efeb71bc50a4e031e4b45
Giancarlos Salas <giansalex@gmail.com>
2020-07-30 22:32:42 -0500

    Use class definition

--- packages/lite/src/Greenter/Factory/XmlBuilderResolver.php
+++ packages/lite/src/Greenter/Factory/XmlBuilderResolver.php
@@ -9,10 +9,6 @@ use Greenter\Builder\BuilderInterface;
 class XmlBuilderResolver
 {
     /**
-     * @var string[]
-     */
-    private $builders;
-    /**
      * @var array
      */
     private $options;
@@ -24,22 +20,19 @@ class XmlBuilderResolver
     public function __construct(array $options = [])
     {
         $this->options = $options;
-        $this->builders = [
-            \Greenter\Model\Sale\Invoice::class => \Greenter\Xml\Builder\InvoiceBuilder::class,
-            \Greenter\Model\Sale\Note::class => \Greenter\Xml\Builder\NoteBuilder::class,
-            \Greenter\Model\Summary\Summary::class => \Greenter\Xml\Builder\SummaryBuilder::class,
-            \Greenter\Model\Voided\Voided::class => \Greenter\Xml\Builder\VoidedBuilder::class,
-            \Greenter\Model\Despatch\Despatch::class => \Greenter\Xml\Builder\DespatchBuilder::class,
-            \Greenter\Model\Retention\Retention::class => \Greenter\Xml\Builder\RetentionBuilder::class,
-            \Greenter\Model\Perception\Perception::class => \Greenter\Xml\Builder\PerceptionBuilder::class,
-            \Greenter\Model\Voided\Reversion::class => \Greenter\Xml\Builder\VoidedBuilder::class,
-        ];
     }
 
     public function find(string $docClass): BuilderInterface
     {
-        $builder = $this->builders[$docClass];
+        $builder = $this->findBuilderType($docClass);
 
         return new $builder($this->options);
+    }
+
+    private function findBuilderType(string $docClass): string
+    {
+        $className = substr(strrchr($docClass, '\\'), 1);
+
+        return "Greenter\\Xml\\Builder\\${className}Builder";
     }
 }
