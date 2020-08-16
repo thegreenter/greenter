8f16e12c6667572cd08d6354b3e21ed35e09ca10
Giancarlos Salas <giansalex@gmail.com>
2020-08-16 12:18:09 -0500

    Add XSL Rules validator

---
+++ packages/cpe-validator/src/Validator/Xml/XslValidator.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenter\Validator\Xml;
+
+use DOMDocument;
+use Greenter\Validator\Entity\CpeError;
+use Greenter\Validator\Parser\ResultParserInterface;
+use InvalidArgumentException;
+use XSLTProcessor;
+
+class XslValidator implements ValidatorInterface
+{
+    /**
+     * @var DOMDocument
+     */
+    private $xslDocument;
+
+    /**
+     * @var ResultParserInterface
+     */
+    private $errorParser;
+
+    /**
+     * XslValidator constructor.
+     *
+     * @param ResultParserInterface $errorParser
+     */
+    public function __construct(ResultParserInterface $errorParser)
+    {
+        $this->errorParser = $errorParser;
+    }
+
+    public function setXsl(DOMDocument $xslDocument)
+    {
+        $this->xslDocument = $xslDocument;
+    }
+
+    /**
+     * @param string $filename
+     * @param DOMDocument $document
+     *
+     * @return CpeError[]
+     */
+    public function validate(string $filename, DOMDocument $document): array
+    {
+        if ($this->xslDocument === null) {
+            throw new InvalidArgumentException('XslDocument no set');
+        }
+
+        $proc = new XsltProcessor();
+
+        $proc->importStylesheet($this->xslDocument);
+        $proc->registerPHPFunctions(['preg_match']);
+        $proc->setParameter('', 'nombreArchivoEnviado', $filename);
+
+        $state = libxml_use_internal_errors(true);
+        $proc->transformToXML($document);
+
+        $errors = $this->getErrors(libxml_get_errors());
+
+        libxml_clear_errors();
+        libxml_use_internal_errors($state);
+
+        return $errors;
+    }
+
+    /**
+     * @param \LibXMLError[]|null $xmlErrors
+     *
+     * @return CpeError[]
+     */
+    private function getErrors(?array $xmlErrors): array
+    {
+        $errors = [];
+        foreach ($xmlErrors as $error) {
+            $errors[] = $this->errorParser->parse($error->message);
+        }
+
+        return $errors;
+    }
+}
