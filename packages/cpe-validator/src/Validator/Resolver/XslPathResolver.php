2d816df5dd0d6aced5abfdcff19216152deca982
Giancarlos Salas <giansalex@gmail.com>
2020-08-17 22:49:37 -0500

    Reduce cyclomatic complexity

--- packages/cpe-validator/src/Validator/Resolver/XslPathResolver.php
+++ packages/cpe-validator/src/Validator/Resolver/XslPathResolver.php
@@ -13,6 +13,19 @@ class XslPathResolver implements RuleResolverInterface
      */
     private $basePath;
 
+    private $pathMapping = [
+        DocumentType::FACTURA => "2.X/ValidaExprRegFactura-2.0.1.xsl",
+        DocumentType::BOLETA => "2.X/ValidaExprRegBoleta-2.0.1.xsl",
+        DocumentType::NOTA_CREDITO => "2.X/ValidaExprRegNC-2.0.1.xsl",
+        DocumentType::NOTA_DEBITO => "2.X/ValidaExprRegND-2.0.1.xsl",
+        DocumentType::RESUMEN_DIARIO => "1.X/ValidaExprRegSummary-1.1.0.xsl",
+        DocumentType::COMUNICACION_BAJA => "1.X/ValidaExprRegSummaryVoided-1.0.1.xsl",
+        DocumentType::GUIA_REMISION => "2.X/ValidaExprRegGuiaRemitente-2.0.1.xsl",
+        DocumentType::RETENCION => "1.X/ValidaExprRegRetencion-1.0.3.xsl",
+        DocumentType::PERCEPCION => "1.X/ValidaExprRegPercepcion-1.0.1.xsl",
+        DocumentType::RESUMEN_REVERSION => "1.X/ValidaExprRegOtrosVoided-1.0.1.xsl",
+    ];
+
     /**
      * XslPathResolver constructor.
      * @param string $basePath
@@ -24,31 +37,10 @@ class XslPathResolver implements RuleResolverInterface
 
     public function getPath(?string $typeDoc): ?string
     {
-        switch ($typeDoc) {
-            case DocumentType::FACTURA: $xslFile = "2.X/ValidaExprRegFactura-2.0.1.xsl";
-                break;
-            case DocumentType::BOLETA: $xslFile = "2.X/ValidaExprRegBoleta-2.0.1.xsl";
-                break;
-            case DocumentType::NOTA_CREDITO: $xslFile = "2.X/ValidaExprRegNC-2.0.1.xsl";
-                break;
-            case DocumentType::NOTA_DEBITO: $xslFile = "2.X/ValidaExprRegND-2.0.1.xsl";
-                break;
-            case DocumentType::RESUMEN_DIARIO: $xslFile = "1.X/ValidaExprRegSummary-1.1.0.xsl";
-                break;
-            case DocumentType::COMUNICACION_BAJA: $xslFile = "1.X/ValidaExprRegSummaryVoided-1.0.1.xsl";
-                break;
-            case DocumentType::GUIA_REMISION: $xslFile = "2.X/ValidaExprRegGuiaRemitente-2.0.1.xsl";
-                break;
-            case DocumentType::RETENCION: $xslFile = "1.X/ValidaExprRegRetencion-1.0.3.xsl";
-                break;
-            case DocumentType::PERCEPCION: $xslFile = "1.X/ValidaExprRegPercepcion-1.0.1.xsl";
-                break;
-            case DocumentType::RESUMEN_REVERSION: $xslFile = "1.X/ValidaExprRegOtrosVoided-1.0.1.xsl";
-                break;
-            default:
-                return null;
+        if (!isset($this->pathMapping[$typeDoc])) {
+            return null;
         }
 
-        return $this->basePath.DIRECTORY_SEPARATOR.$xslFile;
+        return $this->basePath.DIRECTORY_SEPARATOR.$this->pathMapping[$typeDoc];
     }
 }
