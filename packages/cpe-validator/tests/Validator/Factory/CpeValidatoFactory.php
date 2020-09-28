0f0972f9dabc1c2a131c08e88741826d87521513
Giancarlos Salas <giansalex@gmail.com>
2020-09-28 08:58:50 -0500

    Add Cpe Validator factory

---
+++ packages/cpe-validator/tests/Validator/Factory/CpeValidatoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Greenter\Validator\Factory;
+
+use Greenter\Validator\Parser\ErrorResultParser;
+use Greenter\Validator\Resolver\XmlTypeResolver;
+use Greenter\Validator\Resolver\XslPathResolver;
+use Greenter\Validator\Xml\CpeValidator;
+use Greenter\Validator\Xml\XslValidator;
+
+class CpeValidatoFactory
+{
+    public function create(string $xslBasePath): CpeValidator
+    {
+        $typeResolver = new XmlTypeResolver();
+        $rulePathResolver = new XslPathResolver($xslBasePath);
+        $xslValidator = new XslValidator(new ErrorResultParser());
+
+        return new CpeValidator($typeResolver, $rulePathResolver, $xslValidator);
+    }
+}
