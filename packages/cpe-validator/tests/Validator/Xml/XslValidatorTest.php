25fb9a01c991de2fd723ca17235c13f314cf521c
Giancarlos Salas <giansalex@gmail.com>
2020-09-28 08:46:45 -0500

    Add xsl validator tests

---
+++ packages/cpe-validator/tests/Validator/Xml/XslValidatorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Greenter\Validator\Xml;
+
+use DOMDocument;
+use Greenter\Validator\Parser\ResultParserInterface;
+use Greenter\Validator\Xml\XslValidator;
+use InvalidArgumentException;
+use PHPStan\Testing\TestCase;
+
+class XslValidatorTest extends TestCase
+{
+    public function testNotSetXsl()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $validator = new XslValidator($this->createResultParserMock());
+        $docEmpty = new DOMDocument();
+        $validator->validate('a.xml', $docEmpty);
+    }
+
+    private function createResultParserMock(): ResultParserInterface
+    {
+        $stub = $this
+            ->getMockBuilder(ResultParserInterface::class)
+            ->getMock();
+
+        $stub->method('parse')
+            ->willReturn(null);
+
+        /**@var ResultParserInterface $stub */
+        return $stub;
+    }
+}
