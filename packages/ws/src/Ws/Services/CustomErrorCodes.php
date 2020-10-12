5474f1e9d18e1ebe3e4f93d6e0793fec1b47b641
Giancarlos Salas <giansalex@gmail.com>
2020-10-11 19:29:12 -0500

    Add custom errors

---
+++ packages/ws/src/Ws/Services/CustomErrorCodes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenter\Ws\Services;
+
+/**
+ * Class CustomErrorCodes.
+ *
+ * @internal
+ */
+final class CustomErrorCodes
+{
+    public const CDR_NOTFOUND_CODE = 'CDR';
+    public const CDR_NOTFOUND_BILL_MSG = 'CDR no encontrado. La respuesta del servicio de SUNAT no incluyó el CDR.';
+}
