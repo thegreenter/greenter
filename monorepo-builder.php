daae1f70f451e6674df7f4343de5eb321f4b311b
Giancarlos Salas <giansalex@gmail.com>
2021-03-06 10:59:19 -0500

    Update monorepo-builder v9

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,48 +2,41 @@
 
 declare(strict_types=1);
 
+use GreenTools\Monorepo\NextDevReleaseWorker;
+use GreenTools\Monorepo\TagVersionWorker;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
+// monorepo-builder v9
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-
-    # release workers - in order to execute
-    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-    $services->set(AddTagToChangelogReleaseWorker::class);
-    $services->set(TagVersionReleaseWorker::class);
-    $services->set(PushTagReleaseWorker::class);
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
-    $services->set(PushNextDevReleaseWorker::class);
-
     $parameters = $containerConfigurator->parameters();
 
+    $parameters->set(Option::PACKAGE_DIRECTORIES, [
+        __DIR__ . '/packages',
+    ]);
+
     $parameters->set(Option::DATA_TO_APPEND, [
-        'require-dev' => [
+        ComposerJsonSection::REQUIRE_DEV => [
             'phpstan/phpstan' => '^0.12',
             'vimeo/psalm' => '^4.4',
         ],
     ]);
 
-    $parameters->set(Option::DIRECTORIES_TO_REPOSITORIES, [
-        'packages/core' => 'https://github.com/thegreenter/core.git',
-        'packages/cpe-validator' => 'https://github.com/thegreenter/cpe-validator.git',
-        'packages/data' => 'https://github.com/thegreenter/data.git',
-        'packages/htmltopdf' => 'https://github.com/thegreenter/htmltopdf.git',
-        'packages/lite' => 'https://github.com/thegreenter/lite.git',
-        'packages/report' => 'https://github.com/thegreenter/report.git',
-        'packages/validator' => 'https://github.com/thegreenter/validator.git',
-        'packages/ws' => 'https://github.com/thegreenter/ws.git',
-        'packages/xcodes' => 'https://github.com/thegreenter/xcodes.git',
-        'packages/xml' => 'https://github.com/thegreenter/xml.git',
-        'packages/xml-parser' => 'https://github.com/thegreenter/xml-parser.git',
-    ]);
+    $services = $containerConfigurator->services();
+
+    $services->set(UpdateReplaceReleaseWorker::class);
+    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+    $services->set(AddTagToChangelogReleaseWorker::class);
+    $services->set(TagVersionWorker::class);
+    $services->set(SetNextMutualDependenciesReleaseWorker::class);
+    $services->set(UpdateBranchAliasReleaseWorker::class);
+    $services->set(NextDevReleaseWorker::class);
+    $services->set(PushTagReleaseWorker::class);
 };
