35b6d9aa6462e9f0f483962f4a98f6c6d9c5caa8
Giancarlos Salas <giansalex@gmail.com>
2021-03-06 10:49:55 -0500

    Add custom workers for monorepo-builder

---
+++ tools/monorepo/NextDevReleaseWorker.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GreenTools\Monorepo;
+
+use PharIo\Version\Version;
+use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
+use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
+use Symplify\MonorepoBuilder\Utils\VersionUtils;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+use Symplify\PackageBuilder\Parameter\ParameterProvider;
+
+final class NextDevReleaseWorker implements ReleaseWorkerInterface
+{
+    /**
+     * @var ProcessRunner
+     */
+    private $processRunner;
+
+    /**
+     * @var VersionUtils
+     */
+    private $versionUtils;
+
+    /**
+     * @var string
+     */
+    private $branchName;
+
+    public function __construct(
+        ProcessRunner $processRunner,
+        VersionUtils $versionUtils,
+        ParameterProvider $parameterProvider
+    ) {
+        $this->processRunner = $processRunner;
+        $this->versionUtils = $versionUtils;
+        $this->branchName = $parameterProvider->provideStringParameter(Option::DEFAULT_BRANCH_NAME);
+    }
+
+    public function work(Version $version): void
+    {
+        $versionInString = $this->getVersionDev($version);
+
+        $gitAddCommitCommand = sprintf(
+            'git add . && git commit --allow-empty -m "open %s"',
+            $versionInString
+        );
+
+        $this->processRunner->run($gitAddCommitCommand);
+    }
+
+    public function getDescription(Version $version): string
+    {
+        $versionInString = $this->getVersionDev($version);
+
+        return sprintf('Commit next "%s" open', $versionInString);
+    }
+
+    private function getVersionDev(Version $version): string
+    {
+        return $this->versionUtils->getNextAliasFormat($version);
+    }
+}
