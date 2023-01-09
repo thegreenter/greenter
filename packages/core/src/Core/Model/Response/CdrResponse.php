db7cdf54b2265e62724206e536dd2e1a62d49be4
Daniel López Sánchez <dlopezs2009@hotmail.com>
2023-01-09 10:02:54 -0500

    add reference attribute to cdr response model and set reference to DomCdrReader class

--- packages/core/src/Core/Model/Response/CdrResponse.php
+++ packages/core/src/Core/Model/Response/CdrResponse.php
@@ -36,6 +36,11 @@ class CdrResponse
     protected $notes;
 
     /**
+     * @var string|null
+     */
+    protected $reference;
+
+    /**
      * @return string
      */
     public function getId(): ?string
@@ -111,6 +116,24 @@ class CdrResponse
     public function setNotes(?array $notes): CdrResponse
     {
         $this->notes = $notes;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    /**
+     * @param string|null $reference
+     */
+    public function setReference(?string $reference): CdrResponse
+    {
+        $this->reference = $reference;
 
         return $this;
     }
