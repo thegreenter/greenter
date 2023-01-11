b6103c23ea71dd5e6389d83869cbb8b187b3ac65
Giancarlos Salas <me@giansalex.dev>
2023-01-10 20:42:47 -0500

    Set strict types

--- packages/lite/src/Greenter/Api.php
+++ packages/lite/src/Greenter/Api.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Greenter;
 
 use Greenter\Api\ApiFactory;
--- packages/ws/src/Api/ApiFactory.php
+++ packages/ws/src/Api/ApiFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Greenter\Api;
 
 use DateInterval;
